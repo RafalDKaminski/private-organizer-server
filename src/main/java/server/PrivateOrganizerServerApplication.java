c0bd537d35d32e7655285886f4d1fbb7c3158ad1
Rafal Kaminski <rafal.kaminski@capgemini.com>
2022-01-10 21:59:27 +0100

    init commit

---
+++ src/main/java/server/PrivateOrganizerServerApplication.java
@@ -0,0 +1,15 @@
+package server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateOrganizerServerApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(PrivateOrganizerServerApplication.class, args);
+    }
+
+}
---
+++ src/main/java/server/apis/CustomRestController.java
@@ -0,0 +1,18 @@
+package server.apis;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/test")
+public class CustomRestController
+{
+    @GetMapping
+    public String getTestData()
+    {
+        return "Hello World !";
+    }
+
+}
